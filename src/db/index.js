fa148c8b3eb37ccf74afd0f9cbd956b60de00ff4
Anirudh <aniruddhaghosh4109@gmail.com4>
2025-01-14 21:27:49 +0530

    Adding user and video model

--- src/db/index.js
+++ src/db/index.js
@@ -1,17 +1,17 @@
 import mongoose from "mongoose";
- import { DB_NAME } from "../constants.js";
+import { DB_NAME } from "../constants.js";
 
- const  Connect_DB = async ()=>{
-    try {
-       const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
-       console.log(`/n MONGODB CONNECTED !! DB HOST : ${connectionInstance.connection.host} `);
-       
-        
-    } catch (error) {
-        console.log("ERROR: ",error);
+const Connect_DB = async () => {
+   try {
+      const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+      console.log(`/n MONGODB CONNECTED !! DB HOST : ${connectionInstance.connection.host} `);
+
+
+   } catch (error) {
+      console.log("ERROR: ", error);
       process.exit(1)
-        
-    }
- }
 
- export default Connect_DB
+   }
+}
+
+export default Connect_DB
